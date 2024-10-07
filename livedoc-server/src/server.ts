59036db7069b0ef209c41534f66fbaa2536c137c
sohan01fw <sohancwactfw@gmail.com>
2024-10-07 18:58:34 +0545

    successfully configure socket io in server

--- livedoc-server/src/server.ts
+++ livedoc-server/src/server.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import { createServer } from "node:http";
-import { fileURLToPath } from "node:url";
-import path, { dirname, join } from "node:path";
 import { Server } from "socket.io";
 import { PrismaClient } from "@prisma/client";
 
@@ -10,23 +8,13 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-const __dirname = dirname(path.resolve("public/index.html"));
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
-app.post("/user", async (req, res) => {
-  const x = await prisma.user.create({
-    data: {
-      email: "sohan@gmail.com",
-      name: "sohan",
-    },
-  });
-  res.send(x);
-});
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
+
+ io.on("connection", (socket) => {
+  socket.on('msg',(msg)=>{
+    io.emit('msg',msg)
+  })
+}) 
 
 server.listen(8080, () => {
   console.log("server running at http://localhost:8080");
