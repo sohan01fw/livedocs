cfe5dc66a8276f83d0d79a363223071605cfa74c
sohan01fw <sohancwactfw@gmail.com>
2024-10-07 17:17:38 +0545

    add react quill text editor

--- livedoc-client/src/App.tsx
+++ livedoc-client/src/App.tsx
@@ -1,14 +1,11 @@
-import IndexRoute from "./routes"
-
+import IndexRoute from "./routes";
 
 function App() {
   return (
-    <div className="h-screen" data-theme="light" >
-     <IndexRoute />
+    <div className="h-screen" data-theme="light">
+      <IndexRoute />
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
