43ed25d49097783a08bb4d7eeda22f58dca2ffa2
sohan01fw <sohancwactfw@gmail.com>
2024-10-09 09:43:41 +0545

    fix cors error

--- livedoc-server/src/server.ts
+++ livedoc-server/src/server.ts
@@ -6,15 +6,21 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+  cors:{
+    origin:["http://localhost:3000","http://localhost:5173"]
+  }
+});
 
 
 
  io.on("connection", (socket) => {
   socket.on('msg',(msg)=>{
-    io.emit('msg',msg)
+    socket.emit('msg',msg);
   })
+
 }) 
+
 
 server.listen(8080, () => {
   console.log("server running at http://localhost:8080");
