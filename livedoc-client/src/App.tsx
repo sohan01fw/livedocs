fbf96f7b2924145b1d0a9d04e28947f72ca3974f
sohan01fw <sohancwactfw@gmail.com>
2024-10-09 09:43:04 +0545

    successfully execute socket connection

--- livedoc-client/src/App.tsx
+++ livedoc-client/src/App.tsx
@@ -1,9 +1,19 @@
 import IndexRoute from "./routes";
-
+import { MyForm } from "./components/sockets/MyForm";
+import { socket } from "./lib/socket";
+import { useState } from "react";
 function App() {
+  const [value, setvalue] = useState<string>()
+socket.on("msg",(msg)=>{
+  setvalue(msg);
+}) 
   return (
     <div className="h-screen" data-theme="light">
       <IndexRoute />
+      <div className="div">
+        <MyForm />
+        <h3>{value}</h3>
+      </div>
     </div>
   );
 }
