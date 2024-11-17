d3b03125ecfe0a283b3ee9f6876819aed38b45cb
sohan01fw <sohancwactfw@gmail.com>
2024-11-17 18:04:18 +0545

    little optimize

--- livedoc-client/src/lib/socket.ts
+++ livedoc-client/src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080';
+export const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080';
 export const socket = io(URL);
--- livedoc-client/src/main.tsx
+++ livedoc-client/src/main.tsx
@@ -1,20 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-import axios from 'axios'
-
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import axios from "axios";
 
 // Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 axios.defaults.baseURL = "http://localhost:8080";
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
-createRoot(document.getElementById('root')!).render(
-
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <App />
-    </ClerkProvider>
-
-)
+  </ClerkProvider>
+);
